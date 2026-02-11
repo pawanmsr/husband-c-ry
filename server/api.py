11dd50901ae1d79807988ccdc50eeced9ce804d7
Pawan Mishra <pawanmsr@outlook.com>
2026-02-11 14:04:46 +0530

    fix api endpoint

--- server/api.py
+++ server/api.py
@@ -12,31 +12,32 @@ async def root():
     }
 
 @api.get("/agent/nirma/{result}")
-async def nirma():
+async def nirma(result: str):
     return {
         "message": "I am Nirma."
     }
 
 @api.get("/agent/hema/{result}")
-async def hema():
+async def hema(result: str):
     return {
         "message": "My name is Hema."
     }
 
-@api.get("/agent/rekha{result}")
-async def rekha():
+@api.get("/agent/rekha/{result}")
+async def rekha(result: str):
     return {
-        "message": "Most people know me by Rekha."
+        # "message": "Most people know me by Rekha.",
+        "message": agent.respond(result)
     }
 
 @api.get("/agent/jaya/{result}")
-async def jaya():
+async def jaya(result: str):
     return {
         "message": "People usually call me Miss Jaya, but you can call me MJ."
     }
 
 @api.get("/agent/sushma/{result}")
-async def sushma():
+async def sushma(result: str):
     return {
         "message": "Hush up and call me Su."
     }
