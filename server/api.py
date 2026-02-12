5d5e5ba73b5ea1be727ccc5c64de863f1edfd4f7
Pawan Mishra <pawanmsr@outlook.com>
2026-02-12 22:18:49 +0530

    add agency

--- server/api.py
+++ server/api.py
@@ -1,43 +1,50 @@
 from fastapi import FastAPI
+from dotenv import load_dotenv
 
-from agency import agent
-# from agency import nirma, hema, rekha, jaya, sushma
+from agency import Agent
+from agency import NIRMA, HEMA, REKHA, JAYA, SUSHMA
+
+load_dotenv() # access values from os.environ
 
 api = FastAPI()
+agent = Agent()
 
 @api.get("/")
-async def root():
+async def root() -> dict:
     return {
         "message": "Washing Powder"
     }
 
 @api.get("/agent/nirma/{result}")
-async def nirma(result: str):
+async def nirma(result: str) -> dict:
     return {
         "message": "I am Nirma."
     }
 
 @api.get("/agent/hema/{result}")
-async def hema(result: str):
+async def hema(result: str) -> dict:
     return {
         "message": "My name is Hema."
     }
 
 @api.get("/agent/rekha/{result}")
-async def rekha(result: str):
+async def rekha(result: str) -> dict:
+    return agent.respond(result, REKHA)
+
     return {
-        # "message": "Most people know me by Rekha.",
-        "message": agent.respond(result)
+        "message": "Most people know me by Rekha.",
     }
 
 @api.get("/agent/jaya/{result}")
-async def jaya(result: str):
+async def jaya(result: str) -> dict:
+    return agent.respond(result, JAYA)
+
     return {
         "message": "People usually call me Miss Jaya, but you can call me MJ."
     }
 
 @api.get("/agent/sushma/{result}")
-async def sushma(result: str):
+async def sushma(result: str) -> dict:
     return {
         "message": "Hush up and call me Su."
     }
